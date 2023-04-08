7e71ab5ba65fcb8617ec53a98b99aab42586d444
satriawinarah <swinarah@gmail.com>
2023-04-08 16:08:14 +0700

    Template done

--- src/pages/website/home/WebsiteHome.js
+++ src/pages/website/home/WebsiteHome.js
@@ -1,8 +1,18 @@
+import WebsiteFooter from "../../../components/website/footer/WebsiteFooter";
 import WebsiteHeader from "../../../components/website/header/WebsiteHeader";
+import WebsiteMentor from "../../../components/website/mentor/WehsiteMentor";
+import WebsiteReason from "../../../components/website/reason/WebsiteReason";
 
 function WebsiteHome() {
     return (
-        <WebsiteHeader></WebsiteHeader>
+        <div>
+            <WebsiteHeader></WebsiteHeader>
+            <div class="container">
+                <WebsiteReason></WebsiteReason>
+                <WebsiteMentor></WebsiteMentor>
+            </div>
+            <WebsiteFooter></WebsiteFooter>
+        </div>
     );
 }
 
